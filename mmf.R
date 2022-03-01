a0037f7977044470c55a89b9422e42dd03e5ecc2
brandonritchie <rit17003@byui.edu>
2022-03-01 13:01:19 -0700

    .

--- mmf.R
+++ mmf.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-dat <- read_csv("Big_Dance_CSV.csv") %>% 
+dat <- read_csv("https://raw.githubusercontent.com/brandonritchie/march_madness_2022/main/Big_Dance_CSV.csv?token=GHSAT0AAAAAABR4LKX7VAOMCSKP3OA5MWZGYRGKZLQ") %>% 
   rename(Score_1 = Score...6,
          Score_2 = Score...9)
 
@@ -17,6 +17,17 @@ dat2 <- dat %>%
   arrange(desc(n)) %>% 
   rename(Seed_2_Wins = n)
 
-dat1 %>% 
+prob_dat <- dat1 %>% 
   left_join(dat2, by = c("Round", "Seed_1", "Seed_2")) %>% 
-  mutate(prob = Seed_2_Wins / total_matchups)
+  mutate(prob = Seed_2_Wins / total_matchups) %>% 
+  replace_na(list(prob = 0))
+
+sample(c(0,1), size = 1, replace = TRUE, prob = c((1-0.371), 0.371))
+
+seeds_2019 <- read_csv("school_seed_2019.csv")
+
+seed_1 <- seeds_2019 %>% filter(School == "Duke") %>% pull(Seed)
+seed_2 <- seeds_2019 %>% filter(School == "Texas Tech") %>% pull(Seed)
+
+prob_dat %>% filter(Seed_1 == seed_1, Seed_2 == seed_2, Round == 4) %>% pull(prob)
+sample(c(0,1), size = 1,replace = TRUE, prob = )
